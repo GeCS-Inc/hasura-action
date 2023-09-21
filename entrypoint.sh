f9499d884103af8e136065f8c0dbd847e89154a5
Narikawa <38754367+TakaoNarikawa@users.noreply.github.com>
2023-09-21 15:02:27 +0900

    browniefed / hasura-runner から移植

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,39 +1,72 @@
-#!/bin/sh -l
+#!/bin/sh
 
-set -e
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+debug() {
+  printf "ℹ️ ${CYAN}%s${NC}\n", "$1"
+}
+
+warn() {
+  printf "⚠️ ${YELLOW}%s${NC}\n", "$1"
+}
+
+error() {
+  printf "❌ ${RED}%s${NC}\n", "$1"
+}
+
+if [ -n "$HASURA_WORKDIR" ]; then
+    cd $HASURA_WORKDIR
+fi
+
+# If a CLI version was given to Github Action inputs, then use that, otherwise default to latest release
+if [ -z "$HASURA_CLI_VERSION" ]; then
+  warn "Hasura CLI version not provided, downloading latest release"
+  hasura_cli_download_url="https://github.com/hasura/graphql-engine/releases/latest/download/cli-hasura-linux-amd64"
+else
+  debug "Downloading Hasura CLI $HASURA_CLI_VERSION"
+  hasura_cli_download_url="https://github.com/hasura/graphql-engine/releases/download/$HASURA_CLI_VERSION/cli-hasura-linux-amd64"
+fi
+
+# Download the Hasura CLI binary
+wget --quiet --output-document /usr/local/bin/hasura "$hasura_cli_download_url" || {
+  error 'Failed downloading Hasura CLI'
+  exit 1
+}
+
+debug "Making Hasura CLI executable"
+# Make it executable
+chmod +x /usr/local/bin/hasura || {
+  error 'Failed making CLI executable'
+  exit 1
+}
+
 
 if [ -z "$HASURA_ENDPOINT" ]; then
     echo "HASURA_ENDPOINT is required to run commands with the hasura cli"
     exit 126
 fi
 
-command="hasura $* --skip-update-check --endpoint '$HASURA_ENDPOINT'"
-printed_command=$command
+command="hasura $* --endpoint $HASURA_ENDPOINT"
 
 if [ -n "$HASURA_ADMIN_SECRET" ]; then
-    command="$command --admin-secret '$HASURA_ADMIN_SECRET'"
-    # make sure secret is not printed (see issue #15)
-    printed_command="$printed_command --admin-secret '***'" 
+    command="$command --admin-secret $HASURA_ADMIN_SECRET"
 fi
 
-if [ -n "$HASURA_WORKDIR" ]; then
-    cd $HASURA_WORKDIR
-fi
-
-if [ -n "$HASURA_ENGINE_VERSION" ]; then
-    hasura update-cli --version $HASURA_ENGINE_VERSION
+# CD into Hasura project root directory, if given and not current directory
+if [ -n "$PATH_TO_HASURA_PROJECT_ROOT" ]; then
+  debug "cd'ing to Hasura project root at $PATH_TO_HASURA_PROJECT_ROOT"
+  cd $PATH_TO_HASURA_PROJECT_ROOT || {
+    error "Failed to cd into directory $PATH_TO_HASURA_PROJECT_ROOT"
+    exit 1
+  }
 else
-  DETECTED_HASURA_ENGINE_VERSION=$(curl -s "$HASURA_ENDPOINT"/v1/version \
-    | jq .version \
-    | awk '{split($0,a,"-"); print a[1]}' \
-    | awk '{split($0,a,"\""); print a[2]}')
-
-  if [ -n "$DETECTED_HASURA_ENGINE_VERSION" ]; then
-      hasura update-cli --version $DETECTED_HASURA_ENGINE_VERSION
-  fi
+  warn "No path to Hasura project root given, using top-level repo directory"
 fi
 
 # secrets can be printed, they are protected by Github Actions
-echo "Executing $printed_command from ${HASURA_WORKDIR:-./}"
+echo "Executing '$command' from '${PATH_TO_HASURA_PROJECT_ROOT:-./}'"
 
 sh -c "$command"
